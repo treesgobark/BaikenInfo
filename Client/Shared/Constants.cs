5b4f443546c5daa9b23ba6688ea5f0c4e721a4d3
treesgobark <loganpetro97@yahoo.com>
2024-08-21 19:02:08 -0400

    get videos from blob storage now

--- Client/Shared/Constants.cs
+++ Client/Shared/Constants.cs
@@ -2,5 +2,12 @@ namespace Client.Shared;
 
 public static class Constants
 {
-    public static string TitlePrefix { get; } = "How 2 Baiken |";
+    public static string TitlePrefix => "How 2 Baiken |";
+
+    public static string ImageUri(string name) => $"Moves/{name}.png";
+    public static string VideoUri(string name)
+    {
+        name = name.Replace(" ", "%20");
+        return $"https://baikenstorage.blob.core.windows.net/baiken-media/{name}.mp4";
+    }
 }
