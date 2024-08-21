2089543449f43392fcca2ab1bd9dc47a533c1fcd
treesgobark <loganpetro97@yahoo.com>
2024-08-21 15:43:09 -0400

    add mudblazor

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,15 @@
+using Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Client;
+using MudBlazor.Services;
+using MudExtensions.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+builder.Services.AddMudExtensions();
 
 await builder.Build().RunAsync();
